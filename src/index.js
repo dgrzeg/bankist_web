f218e690b7a51fb4a7e727419834cb0634f3604b
dgrzeg <dgr.wroc@gmail.com>
2022-02-14 13:31:51 +0100

    sticky nav

--- src/index.js
+++ src/index.js
@@ -97,3 +97,24 @@ const handleHover = function (e) {
 
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+///////////////////////////////////////
+// sticky nav
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const obsCallback = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) nav.classList.add('sticky');
+    else nav.classList.remove('sticky');
+  });
+};
+
+const obsOptions = {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+};
+const observer = new IntersectionObserver(obsCallback, obsOptions);
+observer.observe(header);
